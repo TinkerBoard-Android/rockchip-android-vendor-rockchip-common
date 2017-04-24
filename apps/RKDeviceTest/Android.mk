a83cbcbb976f65b8c94c9fcb017937d816ec3afb
Huacong Yang <will.yang@rock-chips.com>
2017-04-24 14:09:58 +0800

    RKDeviceTest: don't build LOCAL_MULTILIB := 32 on system app
    this will case other system app crash on 64 bit platform and build user
    image
    
    Change-Id: Idc222884efc46291ad5a0f188ef2b296628c130f
    
    Former-commit-id: 97d6d855f874af6aa37ac075910ff8c7dad34f12

--- apps/RKDeviceTest/Android.mk
+++ apps/RKDeviceTest/Android.mk
@@ -2,7 +2,6 @@
 # RKDeviceTest
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MULTILIB := 32
 LOCAL_MODULE := RKDeviceTest
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
@@ -13,7 +12,11 @@ LOCAL_CERTIFICATE := PRESIGNED
 #LOCAL_OVERRIDES_PACKAGES := 
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 #LOCAL_REQUIRED_MODULES :=
+ifeq ($(strip $(TARGET_ARCH)), arm)
 LOCAL_PREBUILT_JNI_LIBS := \
-    lib/arm/libstlport.so 
-
+    lib/arm/libstlport.so
+else ifeq ($(strip $(TARGET_ARCH)), arm64)
+LOCAL_PREBUILT_JNI_LIBS := \
+    lib/arm64/libstlport.so
+endif
 include $(BUILD_PREBUILT)

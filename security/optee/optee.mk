1abcafd35b79c776ef6c934d69165023d7855e18
Joshua <joshua.huang@rock-chips.com>
2017-05-18 09:26:02 +0800

    add WV keybox provision tool
    Change-Id: Ic3ce1d6a65cd264b4e716c10024ddb08d17ce9ee
    
    Former-commit-id: 8cb6dbafc65c4afa6d5d1bd0c77d79f8f7885904

--- security/optee/optee.mk
+++ security/optee/optee.mk
@@ -33,6 +33,12 @@ PRODUCT_COPY_FILES += \
 
 endif
 endif
+ifeq ($(ENABLE_KEYBOX_PROVISION),true)
+
+PRODUCT_COPY_FILES += \
+	vendor/rockchip/common/security/optee/optee_user/rk_widevine/c11fe8ac-b997-48cf-a28de2a55e5240ef.ta:system/lib/optee_armtz/c11fe8ac-b997-48cf-a28de2a55e5240ef.ta	\
+	vendor/rockchip/common/security/optee/optee_user/rk_widevine/rk_store_keybox:system/bin/rk_store_keybox
+endif
 #LOCAL_PATH := $(call my-dir)
 #OPTEE_KO_FILES := $(shell ls $(LOCAL_PATH)/*.ko)
 #PRODUCT_COPY_FILES += \

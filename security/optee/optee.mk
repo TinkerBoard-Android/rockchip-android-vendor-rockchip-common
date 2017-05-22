27b85f88828ec267349b160905e394cb2789e867
sam.lin <lly@rock-chips.com>
2017-05-22 14:08:00 +0800

    Separate Keymaster and libteec. Tee applies to all platforms.
    Change-Id: Ibe0691e355901394bae99813d151e9e260d370b9
    
    Former-commit-id: aa8fdaf72b792ace76ce63d7c94f3f27f152e611

--- security/optee/optee.mk
+++ security/optee/optee.mk
@@ -2,11 +2,10 @@
 #        vendor/rockchip/common/security/optee/optee.ko:system/lib/modules/optee.ko \
 #	vendor/rockchip/common/security/optee/optee_armtz.ko:system/lib/modules/optee_armtz.ko	
 #ifneq ($(filter rk312x rk3126c, $(strip $(TARGET_BOARD_PLATFORM))), )
-ifneq ($(filter box tablet, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
+ifneq ($(filter tablet, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
 PRODUCT_COPY_FILES += \
 	vendor/rockchip/common/security/optee/ta/258be795-f9ca-40e6-a8699ce6886c5d5d.ta:system/lib/optee_armtz/258be795-f9ca-40e6-a8699ce6886c5d5d.ta	\
 	vendor/rockchip/common/security/optee/ta/0b82bae5-0cd0-49a5-9521516dba9c43ba.ta:system/lib/optee_armtz/0b82bae5-0cd0-49a5-9521516dba9c43ba.ta	\
-	vendor/rockchip/common/security/optee/lib/arm/libteec.so:system/lib/libteec.so	\
 	vendor/rockchip/common/security/optee/lib/arm/libkeymaster2.so:system/lib/libkeymaster2.so	\
 	vendor/rockchip/common/security/optee/lib/arm/libRkTeeKeymaster.so:system/lib/libRkTeeKeymaster.so	\
 	vendor/rockchip/common/security/optee/lib/arm/libkeymaster_messages2.so:system/lib/libkeymaster_messages2.so	\
@@ -18,8 +17,6 @@ PRODUCT_COPY_FILES += \
 
 ifeq ($(strip $(TARGET_ARCH)), arm64)
 PRODUCT_COPY_FILES += \
-	vendor/rockchip/common/security/optee/lib/arm64/tee-supplicant:system/bin/tee-supplicant	\
-	vendor/rockchip/common/security/optee/lib/arm64/libteec.so:system/lib64/libteec.so	\
 	vendor/rockchip/common/security/optee/lib/arm64/libkeymaster2.so:system/lib64/libkeymaster2.so	\
 	vendor/rockchip/common/security/optee/lib/arm64/libRkTeeKeymaster.so:system/lib64/libRkTeeKeymaster.so	\
 	vendor/rockchip/common/security/optee/lib/arm64/libkeymaster_messages2.so:system/lib64/libkeymaster_messages2.so	\
@@ -27,12 +24,19 @@ PRODUCT_COPY_FILES += \
 	vendor/rockchip/common/security/optee/lib/arm64/libRkTeeGatekeeper.so:system/lib64/libRkTeeGatekeeper.so	\
 	vendor/rockchip/common/security/optee/lib/arm64/librkgatekeeper.so:system/lib64/librkgatekeeper.so	\
 	vendor/rockchip/common/security/optee/lib/arm64/gatekeeper.rk30board.so:system/lib64/hw/gatekeeper.rk30board.so
+endif
+endif
+
+ifeq ($(strip $(TARGET_ARCH)), arm64)
+PRODUCT_COPY_FILES += \
+	vendor/rockchip/common/security/optee/lib/arm64/tee-supplicant:system/bin/tee-supplicant        \
+        vendor/rockchip/common/security/optee/lib/arm64/libteec.so:system/lib64/libteec.so
 else
 PRODUCT_COPY_FILES += \
-	vendor/rockchip/common/security/optee/lib/arm/tee-supplicant:system/bin/tee-supplicant
+        vendor/rockchip/common/security/optee/lib/arm/tee-supplicant:system/bin/tee-supplicant	\
+	vendor/rockchip/common/security/optee/lib/arm/libteec.so:system/lib/libteec.so
+endif
 
-endif
-endif
 ifeq ($(ENABLE_KEYBOX_PROVISION),true)
 
 PRODUCT_COPY_FILES += \

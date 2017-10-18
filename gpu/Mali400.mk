46b333db7bf2e78f42e25f18645162f9cda6d2ac
wlq <wlq@rock-chips.com>
2017-10-18 09:56:15 +0800

    BRINGUP: 312x: mali_so of r7p0-00rel1-x-5@0 for Android 8.0 for moving it to vendor_partition
    NOTE:
    	There are some codes must be modified for this inside the mali_so,
    	for changing it's path to /vendor/lib/egl/libGLES_mali.so.
    	So, if you put the former mali_so at /vendor/lib/egl/libGLES_mali.so,
    	it doesn't work.
    
    Change-Id: Ide30e98aa9d208842c14c9b2abb30d3632dc1ff0
    
    Former-commit-id: 31cc3589df65ae4fb4a8b3c83b8e1d4fd018e59c

--- gpu/Mali400.mk
+++ gpu/Mali400.mk
@@ -6,35 +6,35 @@ ifeq ($(strip $(GRAPHIC_MEMORY_PROVIDER)), dma_buf)
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk322x)
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/rk322x/libGLES_mali.so:system/lib/egl/libGLES_mali.so \
+    vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/rk322x/libGLES_mali.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLES_mali.so \
     vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/rk322x/libGLES_mali.so:obj/lib/libGLES_mali.so
 else ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3188)
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/rk3188/libGLES_mali.so:system/lib/egl/libGLES_mali.so \
+    vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/rk3188/libGLES_mali.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLES_mali.so \
     vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/rk3188/libGLES_mali.so:obj/lib/libGLES_mali.so
 else
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/libGLES_mali.so:system/lib/egl/libGLES_mali.so \
+    vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/libGLES_mali.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLES_mali.so \
     vendor/rockchip/common/gpu/Mali400/lib/$(TARGET_ARCH)/libGLES_mali.so:obj/lib/libGLES_mali.so
 endif
 
 ifneq ($(filter rk312x rk3036 rk3188, $(strip $(TARGET_BOARD_PLATFORM))), )
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/Mali400/modules/$(TARGET_ARCH)/$(strip $(TARGET_BOARD_PLATFORM))/mali.ko:system/lib/modules/mali.ko
+    vendor/rockchip/common/gpu/Mali400/modules/$(TARGET_ARCH)/$(strip $(TARGET_BOARD_PLATFORM))/mali.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/mali.ko
 endif
 
 ifneq ($(filter rk3126c rk3128, $(strip $(TARGET_BOARD_PLATFORM))), )
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/Mali400/modules/$(TARGET_ARCH)/rk312x/mali.ko:system/lib/modules/mali.ko
+    vendor/rockchip/common/gpu/Mali400/modules/$(TARGET_ARCH)/rk312x/mali.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/mali.ko
 endif
 
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/gpu_performance/etc/performance_info.xml:system/etc/performance_info.xml \
-    vendor/rockchip/common/gpu/gpu_performance/etc/packages-compat.xml:system/etc/packages-compat.xml \
-    vendor/rockchip/common/gpu/gpu_performance/etc/packages-composer.xml:system/etc/packages-composer.xml \
-    vendor/rockchip/common/gpu/gpu_performance/bin/$(TARGET_ARCH)/performance:system/bin/performance \
-    vendor/rockchip/common/gpu/gpu_performance/lib/$(TARGET_ARCH)/libperformance_runtime.so:system/lib/libperformance_runtime.so \
-    vendor/rockchip/common/gpu/gpu_performance/lib/$(TARGET_ARCH)/gpu.$(TARGET_BOARD_HARDWARE).so:system/lib/hw/gpu.$(TARGET_BOARD_HARDWARE).so
+    vendor/rockchip/common/gpu/gpu_performance/etc/performance_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/performance_info.xml \
+    vendor/rockchip/common/gpu/gpu_performance/etc/packages-compat.xml:$(TARGET_COPY_OUT_VENDOR)/etc/packages-compat.xml \
+    vendor/rockchip/common/gpu/gpu_performance/etc/packages-composer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/packages-composer.xml \
+    vendor/rockchip/common/gpu/gpu_performance/bin/$(TARGET_ARCH)/performance:$(TARGET_COPY_OUT_VENDOR)/bin/performance \
+    vendor/rockchip/common/gpu/gpu_performance/lib/$(TARGET_ARCH)/libperformance_runtime.so:$(TARGET_COPY_OUT_VENDOR)/lib/libperformance_runtime.so \
+    vendor/rockchip/common/gpu/gpu_performance/lib/$(TARGET_ARCH)/gpu.$(TARGET_BOARD_HARDWARE).so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gpu.$(TARGET_BOARD_HARDWARE).so
 
 endif
 endif

9c0dbd11565c5c77cd039530040e7e3f1b17b882
lbt <lbt@rock-chips.com>
2018-01-11 19:50:28 +0800

    wifi display sink:      update libwfdsink_jni.so
    Change-Id: Icce87d882e098334655aa5133974ab80e801e717
    
    Former-commit-id: be2130ea645fa2a47e561baff119618faf10b32b

--- apps/WifiDisplay/Android.mk
+++ apps/WifiDisplay/Android.mk
@@ -12,5 +12,11 @@ LOCAL_CERTIFICATE := PRESIGNED
 #LOCAL_OVERRIDES_PACKAGES := 
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 #LOCAL_REQUIRED_MODULES :=
-LOCAL_PREBUILT_JNI_LIBS := lib/libwfdsink_jni.so
+ifeq ($(strip $(TARGET_ARCH)), arm)
+LOCAL_PREBUILT_JNI_LIBS := \
+    lib/arm/libwfdsink_jni.so
+else ifeq ($(strip $(TARGET_ARCH)), arm64)
+LOCAL_PREBUILT_JNI_LIBS := \
+    lib/arm64/libwfdsink_jni.so
+endif
 include $(BUILD_PREBUILT)

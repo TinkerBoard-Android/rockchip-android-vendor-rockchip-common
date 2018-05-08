afe08257b3a30e4a2bd64fb3fc0897af0cdccf6b
Hery Xu <hery.xu@rock-chips.com>
2018-05-08 12:02:01 +0800

    rk3128h: update media_codecs_performance.xml&media_codecs.xml
    limix 4k
    
    Change-Id: I41e7e1bd53e2f39d5bd7a36c90d0671962426650
    Signed-off-by: Hery Xu <hery.xu@rock-chips.com>
    
    Former-commit-id: ccc25d31429df20514ddadac3c2b85d1ca1f61f1

--- vpu/vpu.mk
+++ vpu/vpu.mk
@@ -138,9 +138,12 @@ PRODUCT_COPY_FILES += \
 else ifneq ($(filter rk3288, $(TARGET_BOARD_PLATFORM)), )
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/vpu/etc/media_codecs_performance_rk3288.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
-else ifneq ($(filter rk322x rk3128h, $(TARGET_BOARD_PLATFORM)), )
+else ifneq ($(filter rk322x, $(TARGET_BOARD_PLATFORM)), )
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/vpu/etc/media_codecs_performance_rk322x.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+else ifneq ($(filter rk3128h, $(TARGET_BOARD_PLATFORM)), )
+PRODUCT_COPY_FILES += \
+    vendor/rockchip/common/vpu/etc/media_codecs_performance_rk3128h.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 else
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/vpu/etc/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
@@ -161,6 +164,9 @@ PRODUCT_COPY_FILES += \
 else ifneq ($(filter rk3328 rk3128h rk322x, $(TARGET_BOARD_PLATFORM)), )
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/vpu/etc/media_codecs_rk3328.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
+else ifneq ($(filter rk3128h, $(TARGET_BOARD_PLATFORM)), )
+PRODUCT_COPY_FILES += \
+    vendor/rockchip/common/vpu/etc/media_codecs_rk3128h.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 else ifneq ($(filter rk%, $(TARGET_BOARD_PLATFORM)), )
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/vpu/etc/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml

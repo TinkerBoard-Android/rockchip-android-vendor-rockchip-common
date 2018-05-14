93f8763569fa3efeed805cb1a3e675e5235a0173
Hery Xu <hery.xu@rock-chips.com>
2018-05-14 10:51:48 +0800

    rk3128h: edit vpu.mk
    rk3128h use media_codecs_rk3128h.xml
    
    Change-Id: Id9666e56d0b1519c1ecf61801d93b4b64dc9d8c4
    Signed-off-by: Hery Xu <hery.xu@rock-chips.com>
    
    Former-commit-id: 3867abb14df7e1fe77ec03bc488dede516e6af35

--- vpu/vpu.mk
+++ vpu/vpu.mk
@@ -166,7 +166,7 @@ PRODUCT_COPY_FILES += \
 else ifneq ($(filter rk3288, $(TARGET_BOARD_PLATFORM)), )
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/vpu/etc/media_codecs_rk3288.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
-else ifneq ($(filter rk3328 rk3128h rk322x, $(TARGET_BOARD_PLATFORM)), )
+else ifneq ($(filter rk3328 rk322x, $(TARGET_BOARD_PLATFORM)), )
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/vpu/etc/media_codecs_rk3328.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 else ifneq ($(filter rk3128h, $(TARGET_BOARD_PLATFORM)), )

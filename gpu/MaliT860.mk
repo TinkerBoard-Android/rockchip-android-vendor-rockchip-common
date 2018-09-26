a69270bbf8c41c29ddf56eddf64152ccbe8162a6
Bian Jin chen <kenjc.bian@rock-chips.com>
2018-09-26 16:54:33 +0800

    GPU: 3326/3399: mali_ko: If the kernel has been compiled, use ko instead of precompiled.
    Change-Id: Ifaa67927cbcaa13a58abec4f75dc3ee773d0a388
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    
    Former-commit-id: 12315bdaf4c7226a8e88662a0417b76cc46d5282

--- gpu/MaliT860.mk
+++ gpu/MaliT860.mk
@@ -3,9 +3,17 @@
 # $(info TARGET_ARCH:$(TARGET_ARCH) )
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM_GPU)), mali-t860)
+DRIVER_PATH := kernel/drivers/gpu/arm/midgard/midgard_kbase.ko
+HAS_BUILD_KERNEL := $(shell test -e $(DRIVER_PATH) && echo true)
+
+ifneq ($(strip $(HAS_BUILD_KERNEL)), true)
 # Move to Android.mk
 BOARD_VENDOR_KERNEL_MODULES += \
 	vendor/rockchip/common/gpu/MaliT860/lib/modules/mali_kbase.ko
+else
+BOARD_VENDOR_KERNEL_MODULES += \
+	$(DRIVER_PATH)
+endif
 endif
 
 PRODUCT_PACKAGES += \

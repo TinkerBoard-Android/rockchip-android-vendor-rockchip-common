3e4e86116d877a0d4f1b7147266df52b512d5441
huangjc <huangjc@rock-chips.com>
2019-12-30 15:34:12 +0800

    vpu:librockit:add conditional compile
    Change-Id: I0ca6e859d693e1d43038f5d7ed3a3025c0f5e421
    
    Former-commit-id: 604a5648c82f39074ae9613b8ed3978fa61f936d

---
+++ vpu/lib/librockit/Android.go
@@ -0,0 +1,38 @@
+package librockit
+
+import (
+        "android/soong/android"
+        "android/soong/cc"
+        "fmt"
+        "strings"
+)
+
+func init() {
+  //该打印会在执行mm命令时，打印在屏幕上
+  fmt.Println("librockit want to conditional Compile")
+  android.RegisterModuleType("cc_librockit_prebuilt_library_shared", DefaultsFactory)
+}
+
+func DefaultsFactory() (android.Module) {
+// 要获取对应module的factory，这一步很重要
+    module := cc.PrebuiltSharedLibraryFactory()
+    android.AddLoadHook(module, Defaults)
+    return module
+}
+
+func Defaults(ctx android.LoadHookContext) {
+    type props struct {
+        Srcs []string
+    }
+    var srcs []string
+    if (strings.Contains(ctx.AConfig().Getenv("TARGET_PRODUCT"),"box")) {
+        srcs = append(srcs,"box/librockit.so")
+//        fmt.Println("is box!!!!")
+    } else {
+        srcs = append(srcs,"librockit.so")
+    }
+
+    p := &props{}
+    p.Srcs = srcs
+    ctx.AppendProperties(p)
+}

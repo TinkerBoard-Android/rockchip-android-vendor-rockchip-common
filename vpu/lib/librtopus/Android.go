5b2729fd1529e1d7346a221f224c64810037ef01
Alex Wang <alex.wang@rock-chips.com>
2019-12-31 16:35:16 +0800

    vpu: libffmpeg_58 and librtopus is needed for box
    Change-Id: Ife285be8b914f8ed97f4abbeacb0a34303ffaf15
    
    Former-commit-id: 5e12989ca23608fa2d8a96af19db968abe0a8116

---
+++ vpu/lib/librtopus/Android.go
@@ -0,0 +1,43 @@
+package librtopus
+
+import (
+        "android/soong/android"
+        "android/soong/cc"
+        "fmt"
+        "strings"
+)
+
+func init() {
+  //该打印会在执行mm命令时，打印在屏幕上
+  fmt.Println("librtopus want to conditional Compile")
+  android.RegisterModuleType("cc_librtopus_prebuilt_library_shared", DefaultsFactory)
+}
+
+func DefaultsFactory() (android.Module) {
+// 要获取对应module的factory，这一步很重要
+    module := cc.PrebuiltSharedLibraryFactory()
+    android.AddLoadHook(module, Defaults)
+    return module
+}
+
+func Defaults(ctx android.LoadHookContext) {
+    type props struct {
+        Srcs []string
+        Vendor  *bool
+    }
+    var srcs []string
+    var vendor bool
+    if (strings.Contains(ctx.AConfig().Getenv("TARGET_PRODUCT"),"box")) {
+        srcs = append(srcs,"librtopus.so")
+        vendor = true
+//        fmt.Println("is box!!!!")
+    } else {
+        srcs = append(srcs,"librtopus.so")
+        vendor = false
+    }
+
+    p := &props{}
+    p.Srcs = srcs
+    p.Vendor = &vendor
+    ctx.AppendProperties(p)
+}

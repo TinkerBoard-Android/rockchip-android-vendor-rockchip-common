4e4d71adfd28cca13e60ba715c85a0601dd273d7
Li Huang <putin.li@rock-chips.com>
2020-05-06 18:14:08 +0800

    GPU: G6110: Correct 32bit path
    Signed-off-by: Li Huang <putin.li@rock-chips.com>
    Change-Id: I680e0c8c2648ce28684fcf025cbad21764e6048f
    
    Former-commit-id: b54b25b9fac3351b045c6cd1103b4df3cb48f972

--- gpu/G6110.mk
+++ gpu/G6110.mk
@@ -68,31 +68,50 @@ PRODUCT_COPY_FILES += \
     vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib64/libclangIMG.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libclangIMG.so
 
 else
+# etc
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/lib/egl/egl.cfg:$(TARGET_COPY_OUT_VENDOR)/lib/egl/egl.cfg
+    vendor/rockchip/common/gpu/libG6110/G6110_64/powervr.ini:$(TARGET_COPY_OUT_VENDOR)/etc/powervr.ini
+
+# lib
+PRODUCT_COPY_FILES += \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/lib/egl/egl.cfg:$(TARGET_COPY_OUT_VENDOR)/lib/egl/egl.cfg
 
 BOARD_VENDOR_KERNEL_MODULES += \
-	vendor/rockchip/common/gpu/libG6110/G6110_32/lib/modules/pvrsrvkm.ko
+	vendor/rockchip/common/gpu/libG6110/G6110_64/lib/modules/pvrsrvkm.ko
+
+# vendor/firmware
+PRODUCT_COPY_FILES += \
+   vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/firmware/rgx.fw.signed.5.9.1.46:$(TARGET_COPY_OUT_VENDOR)/firmware/rgx.fw.signed.5.9.1.46 \
+   vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/firmware/rgx.fw.signed.5.9.1.46:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rgx.fw.signed.5.9.1.46
 
 # vendor/bin
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/bin/pvrsrvctl:$(TARGET_COPY_OUT_VENDOR)/bin/pvrsrvctl \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/bin/pvrtld:$(TARGET_COPY_OUT_VENDOR)/bin/pvrtld
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/bin/pvrsrvctl:$(TARGET_COPY_OUT_VENDOR)/bin/pvrsrvctl \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/bin/pvrtld:$(TARGET_COPY_OUT_VENDOR)/bin/pvrtld
 
+# Vulkan support, use swiftshader when supporting GMS.
+ifneq ($(strip $(BUILD_WITH_GOOGLE_MARKET)), true)
+PRODUCT_COPY_FILES += \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/hw/vulkan.rk3368.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/vulkan.rk3368.so
+endif
 # vendor/lib
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/egl/libEGL_POWERVR_ROGUE.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libEGL_POWERVR_ROGUE.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/egl/libGLESv1_CM_POWERVR_ROGUE.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLESv1_CM_POWERVR_ROGUE.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/egl/libGLESv2_POWERVR_ROGUE.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLESv2_POWERVR_ROGUE.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/hw/gralloc.rk3368.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gralloc.rk3368.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libcreatesurface.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcreatesurface.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libglslcompiler.so:$(TARGET_COPY_OUT_VENDOR)/lib/libglslcompiler.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libIMGegl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libIMGegl.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libpvrANDROID_WSEGL.so:$(TARGET_COPY_OUT_VENDOR)/lib/libpvrANDROID_WSEGL.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libsrv_init.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsrv_init.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libsrv_um.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsrv_um.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libusc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libusc.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libPVROCL.so:$(TARGET_COPY_OUT_VENDOR)/lib/libPVROCL.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/liboclcompiler.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboclcompiler.so \
-    vendor/rockchip/common/gpu/libG6110/G6110_32/vendor/lib/libufwriter.so:$(TARGET_COPY_OUT_VENDOR)/lib/libufwriter.so
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/egl/libEGL_POWERVR_ROGUE.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libEGL_POWERVR_ROGUE.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/egl/libGLESv1_CM_POWERVR_ROGUE.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLESv1_CM_POWERVR_ROGUE.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/egl/libGLESv2_POWERVR_ROGUE.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLESv2_POWERVR_ROGUE.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/hw/gralloc.rk3368.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gralloc.rk3368.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/hw/memtrack.rk3368.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/memtrack.rk3368.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libcreatesurface.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcreatesurface.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libglslcompiler.so:$(TARGET_COPY_OUT_VENDOR)/lib/libglslcompiler.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libIMGegl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libIMGegl.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libpvrANDROID_WSEGL.so:$(TARGET_COPY_OUT_VENDOR)/lib/libpvrANDROID_WSEGL.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libPVRScopeServices.so:$(TARGET_COPY_OUT_VENDOR)/lib/libPVRScopeServices.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libsrv_um.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsrv_um.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libusc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libusc.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libPVROCL.so:$(TARGET_COPY_OUT_VENDOR)/lib/libPVROCL.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/liboclcompiler.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboclcompiler.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libufwriter.so:$(TARGET_COPY_OUT_VENDOR)/lib/libufwriter.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libLLVMIMG.so:$(TARGET_COPY_OUT_VENDOR)/lib/libLLVMIMG.so \
+    vendor/rockchip/common/gpu/libG6110/G6110_64/vendor/lib/libclangIMG.so:$(TARGET_COPY_OUT_VENDOR)/lib/libclangIMG.so
+
 endif

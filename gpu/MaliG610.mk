fd2856a0b8934671879cbcddf065a11c7b83f631
Zhen Chen <chenzhen@rock-chips.com>
2021-12-11 15:51:17 +0800

    GPU: 3588: MaliG610.mk: install mali_csffw.bin
    Change-Id: I6f49b2f650444c8cbe5f8901093e9ab565c7da97

--- gpu/MaliG610.mk
+++ gpu/MaliG610.mk
@@ -3,4 +3,7 @@ ifeq ($(strip $(TARGET_BOARD_PLATFORM_GPU)), mali-G610)
 PRODUCT_PACKAGES += \
         libGLES_mali \
         vulkan.$(TARGET_BOARD_PLATFORM)
+
+PRODUCT_COPY_FILES += \
+	vendor/rockchip/common/gpu/MaliG610/firmware/mali_csffw.bin:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/mali_csffw.bin
 endif

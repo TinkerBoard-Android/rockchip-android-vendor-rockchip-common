2f36fb785d1b59e69e1aadcfa9fa674da56a390d
Kunyang_Fan <Kunyang_Fan@asus.com>
2024-03-29 09:18:34 +0800

    Wifi: Add RTW88 wifi firmwares and change the modules path
    Change-Id: I6584bab356208e0d2a55724507e669ef12e2e70a
    Signed-off-by: Kunyang_Fan <Kunyang_Fan@asus.com>

--- wifi/wifi.mk
+++ wifi/wifi.mk
@@ -12,10 +12,10 @@ $(shell for line in $(EXT_WIFI_DRIVER); do rm $(TOPDIR)$(PRODUCT_KERNEL_PATH)/dr
 
 endif
 
-WIFI_KO_FILES := $(shell find $(TOPDIR)$(PRODUCT_KERNEL_PATH)/drivers/net/wireless/rockchip_wlan -name "*.ko" -type f)
+WIFI_KO_FILES := $(shell find $(TOPDIR)$(PRODUCT_KERNEL_PATH)/drivers/net/wireless -name "*.ko" -type f)
 BOARD_VENDOR_KERNEL_MODULES += \
 	$(foreach file, $(WIFI_KO_FILES), $(file))
 
-WifiFirmwareFile := $(shell ls $(CUR_PATH)/wifi/firmware)
-PRODUCT_COPY_FILES += \
+#WifiFirmwareFile := $(shell ls $(CUR_PATH)/wifi/firmware)
+#PRODUCT_COPY_FILES += \
         $(foreach file, $(WifiFirmwareFile), $(CUR_PATH)/wifi/firmware/$(file):$(TARGET_COPY_OUT_VENDOR)/etc/firmware/$(file))
